243ffbadfe48972c53d26135e8dcfafd674a9987
Amit0129 <amitkumarnayak40@gmail.com>
2022-10-28 20:47:22 +0530

    Addded: Ability to create a Contacts in Address Book with first and last names, address,etc

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,10 +1,56 @@
-﻿namespace AddressBook
+﻿using AddressBook.Model;
+using AddressBook.Repository;
+
+namespace AddressBook
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
+
+            Console.WriteLine("Enter First Name");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Enter Address");
+            string address = Console.ReadLine();
+
+            Console.WriteLine("Enter City Name");
+            string city = Console.ReadLine();
+
+
+            Console.WriteLine("Enter State Name");
+            string state = Console.ReadLine();
+
+            Console.WriteLine("Enter  Zip number");
+            string zip = Console.ReadLine();
+
+            Console.WriteLine("Enter Phone Number");
+            string pnoneNumber = Console.ReadLine();
+
+            Console.WriteLine("Enter Email");
+            string email = Console.ReadLine();
+
+            ContactDetails contactDetail = new ContactDetails()
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Address = address,
+                City = city,
+                State = state,
+                Zip = zip,
+                PhoneNumber = pnoneNumber,
+                Email = email
+            };
+            ContactDetailsRepository contactDetails = new ContactDetailsRepository();
+            contactDetails.AddContactDetails(contactDetail);
+            
+
+            contactDetails.DisplayContact();
+            
         }
     }
 }
---
+++ AddressBook/Repository/ContactDetailsRepository.cs
@@ -0,0 +1,44 @@
+﻿using AddressBook.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook.Repository
+{
+    internal class ContactDetailsRepository
+    {
+        Dictionary<string, ContactDetails> contactDetails = new Dictionary<string, ContactDetails>();
+
+        public void AddContactDetails(ContactDetails contactDetail)
+        {
+            string? phoneNumber = contactDetail.PhoneNumber;
+            contactDetails.Add(phoneNumber, contactDetail);
+            //=====================KEY=========VALUE=======
+        }
+        //=======================OR=========================================
+        /*public void AddContactDetails(string firstname, string lastname, string mobilenumber)
+        {
+            ContactDetails contactDetail = new ContactDetails();
+            contactDetail.FirstName = firstname;
+            contactDetail.LastName = lastname;
+            contactDetail.MobileNumber = mobilenumber;
+            contactDetails.Add(contactDetail.MobileNumber, contactDetail);
+            //========================KEY==================VALUES=======
+        }*/
+        public void DisplayContact()
+        {
+            foreach(var item in contactDetails)
+            {
+                Console.WriteLine($"Contact details of :: {item.Value.FirstName}");
+                Console.WriteLine($"Contact details of :: {item.Value.LastName}");
+                Console.WriteLine($"Contact details of :: {item.Value.PhoneNumber}");
+                Console.WriteLine($"Contact details of :: {item.Value.Email}");
+                Console.WriteLine($"Contact details of :: {item.Value.State}");
+                Console.WriteLine($"Contact details of :: {item.Value.City}");
+                Console.WriteLine($"Contact details of :: {item.Value.Zip}");
+            }
+        }
+    }
+}
