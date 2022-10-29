a156118ada2c44a371a6fc446d7391bfa1f627fa
Amit0129 <amitkumarnayak40@gmail.com>
2022-10-29 08:22:17 +0530

    Added:Ablity to Add New Contact Deatils

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -7,50 +7,69 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Program");
+            bool check = true;
+            
 
-            Console.WriteLine("Enter First Name");
-            string firstName = Console.ReadLine();
-
-            Console.WriteLine("Enter Last Name");
-            string lastName = Console.ReadLine();
-
-            Console.WriteLine("Enter Address");
-            string address = Console.ReadLine();
-
-            Console.WriteLine("Enter City Name");
-            string city = Console.ReadLine();
-
-
-            Console.WriteLine("Enter State Name");
-            string state = Console.ReadLine();
-
-            Console.WriteLine("Enter  Zip number");
-            string zip = Console.ReadLine();
-
-            Console.WriteLine("Enter Phone Number");
-            string pnoneNumber = Console.ReadLine();
-
-            Console.WriteLine("Enter Email");
-            string email = Console.ReadLine();
-
-            ContactDetails contactDetail = new ContactDetails()
+            while (check)
             {
-                FirstName = firstName,
-                LastName = lastName,
-                Address = address,
-                City = city,
-                State = state,
-                Zip = zip,
-                PhoneNumber = pnoneNumber,
-                Email = email
-            };
-            ContactDetailsRepository contactDetails = new ContactDetailsRepository();
-            contactDetails.AddContactDetails(contactDetail);
-            
 
-            contactDetails.DisplayContact();
-            
+                Console.WriteLine("Welcome to Address Book Program");
+
+                Console.WriteLine("Enter First Name");
+                string firstName = Console.ReadLine();
+
+                Console.WriteLine("Enter Last Name");
+                string lastName = Console.ReadLine();
+
+                Console.WriteLine("Enter Address");
+                string address = Console.ReadLine();
+
+                Console.WriteLine("Enter City Name");
+                string city = Console.ReadLine();
+
+
+                Console.WriteLine("Enter State Name");
+                string state = Console.ReadLine();
+
+                Console.WriteLine("Enter  Zip number");
+                string zip = Console.ReadLine();
+
+                Console.WriteLine("Enter Phone Number");
+                string pnoneNumber = Console.ReadLine();
+
+                Console.WriteLine("Enter Email");
+                string email = Console.ReadLine();
+
+                ContactDetails contactDetail = new ContactDetails()
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Address = address,
+                    City = city,
+                    State = state,
+                    Zip = zip,
+                    PhoneNumber = pnoneNumber,
+                    Email = email
+                };
+                ContactDetailsRepository contactDetails = new ContactDetailsRepository();
+                contactDetails.AddContactDetails(contactDetail);
+
+
+                contactDetails.DisplayContact();
+                Console.WriteLine("Enter y if u want more Contact Deatils");
+                string dicision = Console.ReadLine();
+                if(dicision == "y")
+                {
+                    check = true;
+                }
+                else
+                {
+                    check = false;
+                }
+
+
+            }
+
         }
     }
 }
