78c7d09465031af64ff28416f14c150ee26c74d0
Amit0129 <amitkumarnayak40@gmail.com>
2022-11-02 00:22:52 +0530

    Added UC3 Ablity to Update Contact details

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,5 +1,6 @@
 ﻿using AddressBook.Model;
 using AddressBook.Repository;
+using System.Transactions;
 
 namespace AddressBook
 {
@@ -7,6 +8,7 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
+            ContactDetailsRepository contactDetails = new ContactDetailsRepository();
             bool check = true;
             
 
@@ -51,7 +53,7 @@ namespace AddressBook
                     PhoneNumber = pnoneNumber,
                     Email = email
                 };
-                ContactDetailsRepository contactDetails = new ContactDetailsRepository();
+                
                 contactDetails.AddContactDetails(contactDetail);
 
 
@@ -69,6 +71,57 @@ namespace AddressBook
 
 
             }
+            Console.WriteLine("Enter persons First Naame if you want to change the contact details");
+            string name = Console.ReadLine();
+            Console.WriteLine("To change First name Enter 1 \n To change Last Name Enter 2 \n To change Address Enter 3 \n To change City nameEnter 4 \n To change State Enter 5 \n To change Zip enter 6 \n To change Phone Number ebter 7 \n To change email enter 8  ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Enter new First Name u Want to Update");
+                    string fName = Console.ReadLine();
+                    contactDetails.EditContactFirstName(name, fName);
+                    break;
+                case 2:
+                    Console.WriteLine("Enter new Last Name You Want to Update");
+                    string lName = Console.ReadLine();
+                    contactDetails.EditContactLastName(name, lName);
+                    break;
+                case 3:
+                    Console.WriteLine("Enter new Address You Want to Update");
+                    string aDress = Console.ReadLine();
+                    contactDetails.EditContactAdress(name, aDress);
+                    break;
+                case 4:
+                    Console.WriteLine("Enter new City Name You Want to Update");
+                    string cIty = Console.ReadLine();
+                    contactDetails.EditContactCity(name, cIty);
+                    break;
+                case 5:
+                    Console.WriteLine("Enter new State Want to Update");
+                    string stat = Console.ReadLine();
+                    contactDetails.EditContactState(name, stat);
+                    break;
+                case 6:
+                    Console.WriteLine("Enter new Zip code Want to Update");
+                    string zipCo = Console.ReadLine();
+                    contactDetails.EditContactZip(name, zipCo);
+                    break;
+                case 7:
+                    Console.WriteLine("Enter new phone number you Want to Update");
+                    string pno = Console.ReadLine();
+                    contactDetails.EditContactPhoneNumber(name, pno);
+                    break;
+                case 8:
+                    Console.WriteLine("Enter new Email Id You Want to Update");
+                    string emaill = Console.ReadLine();
+                    contactDetails.EditContactEmail(name, emaill);
+                    break;
+            }
+            Console.WriteLine("Enter persons First Naame if you want remove the contact details");
+            string rName = Console.ReadLine();
+            contactDetails.RemoveContactDateils(rName);
+
 
         }
     }
--- AddressBook/Repository/ContactDetailsRepository.cs
+++ AddressBook/Repository/ContactDetailsRepository.cs
@@ -40,5 +40,87 @@ namespace AddressBook.Repository
                 Console.WriteLine($"Contact details of :: {item.Value.Zip}");
             }
         }
+        public ContactDetails GetFirstName(string firstname)
+        {
+            foreach (var item in contactDetails)
+            {
+                if(item.Value.FirstName.Contains(firstname))
+                {
+                    return item.Value;
+                }
+            }
+            return null;
+        }
+        public void EditContactFirstName(string firstname,string firstN)
+        {
+            var personDetail = GetFirstName(firstname);
+            contactDetails.Remove(personDetail.PhoneNumber);
+            personDetail.FirstName = firstN;
+            contactDetails.Add(personDetail.PhoneNumber, personDetail);
+
+        }
+        public void EditContactLastName(string firstname, string lastN)
+        {
+            var personDetail = GetFirstName(firstname);
+            contactDetails.Remove(personDetail.PhoneNumber);
+            personDetail.LastName = lastN;
+            contactDetails.Add(personDetail.PhoneNumber, personDetail);
+
+        }
+        public void EditContactAdress(string firstname, string addrs)
+        {
+            var personDetail = GetFirstName(firstname);
+            contactDetails.Remove(personDetail.PhoneNumber);
+            personDetail.Address = addrs;
+            contactDetails.Add(personDetail.PhoneNumber, personDetail);
+
+        }
+        public void EditContactCity(string firstname, string cit)
+        {
+            var personDetail = GetFirstName(firstname);
+            contactDetails.Remove(personDetail.PhoneNumber);
+            personDetail.City = cit;
+            contactDetails.Add(personDetail.PhoneNumber, personDetail);
+
+        }
+        public void EditContactState(string firstname, string ste)
+        {
+            var personDetail = GetFirstName(firstname);
+            contactDetails.Remove(personDetail.PhoneNumber);
+            personDetail.State = ste;
+            contactDetails.Add(personDetail.PhoneNumber, personDetail);
+
+        }
+        public void EditContactZip(string firstname, string zi)
+        {
+            var personDetail = GetFirstName(firstname);
+            contactDetails.Remove(personDetail.PhoneNumber);
+            personDetail.Zip = zi;
+            contactDetails.Add(personDetail.PhoneNumber, personDetail);
+
+        }
+        public void EditContactPhoneNumber(string firstname, string phNo)
+        {
+            var personDetail = GetFirstName(firstname);
+            contactDetails.Remove(personDetail.PhoneNumber);
+            personDetail.PhoneNumber = phNo;
+            contactDetails.Add(personDetail.PhoneNumber, personDetail);
+
+        }
+        public void EditContactEmail(string firstname, string emai)
+        {
+            var personDetail = GetFirstName(firstname);
+            contactDetails.Remove(personDetail.PhoneNumber);
+            personDetail.Email = emai;
+            contactDetails.Add(personDetail.PhoneNumber, personDetail);
+
+        }
+        public void RemoveContactDateils(string firstname)
+        {
+            var personDetail = GetFirstName(firstname);
+            contactDetails.Remove(personDetail.PhoneNumber);
+            
+
+        }
     }
 }
